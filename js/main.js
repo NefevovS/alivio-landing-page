b52a60f7393dd1a10fc6990045ff8620309b6d4e
Nefedov Sergey <nefedov38@gmail.com>
2022-09-16 15:06:38 +0300

    comlete

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const mobileNavButton = document.querySelector(".mobile-nav-button");
 const mobileNavButtonIcon = document.querySelector(".mobile-nav-button__icon");
+const mobileNav = document.querySelector(".mobile-nav ");
 
 mobileNavButton.addEventListener("click", function () {
   mobileNavButtonIcon.classList.toggle("active");
+  mobileNav.classList.toggle("active");
+  document.body.classList.toggle("no-scroll");
 });
