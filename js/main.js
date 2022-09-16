20175539c2ce1e6e561aad603342460eae1a11b3
Sergey Nefedov <nefedov38@gmail.com>
2022-09-16 20:43:27 +0300

    video

--- js/main.js
+++ js/main.js
@@ -7,3 +7,30 @@ mobileNavButton.addEventListener("click", function () {
   mobileNav.classList.toggle("active");
   document.body.classList.toggle("no-scroll");
 });
+
+const videoBtn = document.querySelector("#video-story-btn");
+const videoFile = document.querySelector("#video-story");
+const videoBtnIcon = document.querySelector("#video-story-btn-icon");
+const videoOverlay = document.querySelector("#video-story-overlay");
+
+function toggleOverlay(event) {
+  if (event.type === "mouseleave") {
+    videoOverlay.classList.add("hidden");
+  } else {
+    videoOverlay.classList.remove("hidden");
+  }
+}
+
+videoBtn.addEventListener("click", function () {
+  if (videoFile.paused) {
+    videoFile.play();
+    videoOverlay.onmouseleave = toggleOverlay;
+    videoOverlay.onmouseenter = toggleOverlay;
+    videoBtnIcon.src = "./img/pause_icon.svg";
+  } else {
+    videoFile.pause();
+    videoBtnIcon.src = "./img/play-white.svg";
+    videoOverlay.onmouseleave = null;
+    videoOverlay.onmouseenter = null;
+  }
+});
